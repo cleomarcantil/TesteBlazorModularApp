4240a6835bd824f2b50b78e184978bb1d5c46ebf
Cleomar Cantil <cleomarcantil@gmail.com>
2024-07-06 23:56:04 -0300

    Configuração para gerar css ao debugar no modo Production.

---
+++ TesteBlazorModularApp.Area1/Pages/Teste.razor.cs
@@ -0,0 +1,9 @@
+
+namespace TesteBlazorModularApp.Area1.Pages;
+
+public partial class Teste
+{
+
+    public string Data { get; set; } = DateTime.Now.ToString();
+
+}
--- TesteBlazorModularApp/Program.cs
+++ TesteBlazorModularApp/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore;
 using TesteBlazorModularApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,14 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+if (!builder.Environment.IsDevelopment() && System.Diagnostics.Debugger.IsAttached)
+{
+    builder.WebHost.UseStaticWebAssets();
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
